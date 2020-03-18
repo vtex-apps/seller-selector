ceed49281c460a98e3ec1374d284fe3c2494c7d0
Everton Ataide <everton.ataide@vtex.com.br>
2020-03-18 14:49:05 -0300

    Simulate Shipping

---
+++ react/SellerShipping.tsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react'
+import CurrentSellerContext from './CurrentSellerContext'
+import { FormattedMessage } from 'react-intl'
+import { FormattedCurrency } from 'vtex.format-currency'
+import TranslateEstimate from 'vtex.shipping-estimate-translator/TranslateEstimate'
+
+const SellerShipping: StorefrontFunctionComponent<any> = ({}) => {
+  const { shipping } = useContext(CurrentSellerContext)
+
+  console.log(shipping)
+
+  return (
+    <div className="items-center tc w-100-s w-20-m br2 ph6 pv4 ma0 w-100-s w-20-m">
+      {shipping ? (
+        shipping.slas.map((sla: any, index: number) => (
+          <p key={index}>
+            {sla.name} <FormattedCurrency value={sla.price / 100} />{' '}
+            <TranslateEstimate shippingEstimate={sla.shippingEstimate} />
+          </p>
+        ))
+      ) : (
+        <FormattedMessage id="store/seller-list.pending" />
+      )}
+    </div>
+  )
+}
+
+export default SellerShipping
