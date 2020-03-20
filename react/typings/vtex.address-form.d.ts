d89195c62df704fd1dfd815289b72d13c7e42958
Everton Ataide <everton.ataide@vtex.com.br>
2020-03-20 15:08:26 -0300

    Set address  form, lacking styles

---
+++ react/typings/vtex.address-form.d.ts
@@ -0,0 +1,12 @@
+declare module 'vtex.address-form'
+
+declare module 'vtex.address-form/inputs' {
+  export const StyleguideInput
+  export const GeolocationInput
+}
+
+declare module 'vtex.address-form/helpers' {
+  export const addValidation
+  export const removeValidation
+  export const injectRules
+}
